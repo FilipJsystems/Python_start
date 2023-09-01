b3b5b0b2eaad7c0680c16362bed2ff09a35bda56
unknown <szkolenia_python@proton.me>
2023-09-01 19:22:50 +0200

    mala zmiana

--- main.py
+++ main.py
@@ -10,7 +10,17 @@ def print_hi(name):
 
 
 # Press the green button in the gutter to run the script.
+# mala zmiana - Fibbo:
 if __name__ == '__main__':
     print_hi('PyCharm')
+    x = 1
+    y = 0
+    for i in range(1,20):
+        z = x + y
+        y = x
+        x = z
+        print(z)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
