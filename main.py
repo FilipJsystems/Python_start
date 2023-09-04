f3ed7203edaa471707f935561d9d3335319b9473
unknown <szkolenia_python@proton.me>
2023-09-04 11:31:54 +0200

    pierwsze zadanie

--- main.py
+++ main.py
@@ -1,6 +1,51 @@
 # Zaczynamy szkolenie!
 
-if __name__ == '__main__':
-    print('Hello World!')
+#if __name__ == '__main__':
+
+
+print('Hello World!')
+
+x = 4
+x2 = 2
+y = 3.0
+z = '3'
+
+# print('hello' + str(x) )   # ok
+# print(3 +  int(z) )   # ok
+# print(3 + float(z))   # ok
+#
+#
+# print('hello', x )   # ok
+# print(x, type(x))
+# print(y, type(y))
+# print(z, type(z))
+#
+# print(x/x2)
+# print(x//x2)
+
+#########
+
+if x == 3:
+    print('tak, x = 4')
+else:
+    print('nie, x nie jest równy 4')
+
+x = input('podaj swoj wiek')
+
+# Napisz program który przyjmie od użyszkodnika imię oraz nazwisko, a następnie
+# wypisze na konsoli komunikat typu "Witaj TwojeImie TwojeNazwisko!"
+
+
+
+
+
+
+
+
+
+
+# w terminalu, instalacja biblioteki: pip install flask
+import flask
+
 
 
