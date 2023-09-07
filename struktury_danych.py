537645dd41ef5481e211fae331de1a5c4c51fcc2
unknown <szkolenia_python@proton.me>
2023-09-07 14:10:11 +0200

    slowniki -2

--- struktury_danych.py
+++ struktury_danych.py
@@ -273,6 +273,10 @@ print(dic)
 
 # Wczytaj do słownika dane z pliku dane.txt (csv) tak by kluczem było imię sklejone z nazwiskiem rozdzielone spacja, a pozostałe dane znalazły się w wartości
 # jako krotka lub lista. Przeiteruj po slowniku i wyswietl jego zawartość.
+#{'Filip Waszkiewicz': [masa, wzrost... ], ... }
+# lista[-1] - ostatni element listy
+# lista[0:3] - pozostałe elementy
+# lista[:-1] - wszystkie elementy poza ostatnim
 
 
 
