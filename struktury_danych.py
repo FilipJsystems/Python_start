e984b1fbba4e005eae92aeebd01109e8cddbb06d
unknown <szkolenia_python@proton.me>
2023-09-07 09:43:18 +0200

    zbiory (sets)

--- struktury_danych.py
+++ struktury_danych.py
@@ -46,4 +46,78 @@ with open(filename) as f:
         ListaKoncowa.append(NowaKrotka)
 print(ListaKoncowa)
 
-################
+################ Zestawy (sets) - zbiory
+
+x = {}          # dict
+print(type(x))  # słownik
+
+x = {1,2,3,4}
+print(type(x))  # set (zbiór)
+
+x = {1:2, 2:3}  # słownik (dict)
+print(type(x))
+
+# bezpieczna metoda deklaracji zbiorów (zestawów) i słowników - jeżeli są puste
+
+x = set()       # tak tworzymy zbiór pusty
+print(type(x))
+
+x = dict()
+print(type(x))
+
+####################################
+
+x = (1)
+print(type(x)) # dalej int
+
+x = ({1,2,3,4})
+print(type(x))  # dalej set (zbiór)
+
+x = ({1:2, 2:3})  # dalej słownik (dict)
+print(type(x))
+
+##### wyjaśnienie - krotka 1-elementowa przechodzi natychmiastiowo w ten jeden element
+# - nie ma zbyt dużego sensu używac 1-elementowych krotek ponieważ one się nie zmieniaja, więc wartość i tak zostanie ta sama.
+
+x = [1]
+print(type(x))  # w tym przypadku jest to lista, ponieważ list jest mutowalna
+
+
+x = ({1,2,3,4}, {5,6})
+print(type(x))          # tutaj już jest krotka (ponieważ ma więcej niż jeden element)
+
+
+####################### głowna cecha zbiorów - to jest unikalnośc elemntów - z tego względu używa się do deduplikacji
+
+l = [1,1,1,1,2,3,4,5]
+
+print(set(l))
+
+print(l)
+l = list(set(l))  # przydatny trick na pozbycie dupliaktów.
+print(l)
+
+##### działania na zbiorach:
+
+set1  = set([1,2,3,3,4])
+set1 = {1,2,3,3,4}
+
+set2 = {3,4,5,6}
+
+print(set1.intersection(set2))  # część wspólna zbiorów
+print(set1 & set2)      #część wspólna zbiorów - inny zapis
+print(set1 and set2)  # tak nie robimy bo to nnie ma sensu
+
+print(set1.union(set2))  # suma zbiorów
+print(set1 | set2)      # suma zbiorów zbiorów - inny zapis
+print(set1 or set2)  # tak nie robimy bo to nnie ma sensu
+
+#   A u B = A + B - A^B
+# A \ B = A - A^B
+print(set1.difference(set2))  # rożnica zbiorów
+print(set1 - set2)      # róznica zbiorów zbiorów - inny zapis
+
+# Wygeneruj dwa zestawy, dodaj do nich po 20 (w przypadku duplikatów lista może być mniejsza niż 20 elementów) losowych liczb z zakresu 1-40.
+# Wyswietl ich sumę, różnicę i część wspólną
+
+
